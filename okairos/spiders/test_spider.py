6a23ffd8587e11dd888e870d7deac4107dfcff45
Vasilis Koulouris <basileskouloures@gmail.com>
2024-04-01 12:17:23 +0300

    fixing test_spider.py

--- okairos/spiders/test_spider.py
+++ okairos/spiders/test_spider.py
@@ -7,12 +7,11 @@ from ..items import OkairosItem
 
 class OkairosSpiderSpider(scrapy.Spider):
     name = 'okairos_spider'
-    allowed_domains = [
-        'https://www.okairos.gr/%CF%84%CF%81%CE%AF%CF%80%CE%BF%CE%BB%CE%B7.html?v=%CF%89%CF%81%CE%B9%CE%B1%CE%AF%CE%B1']
-    start_urls = [
-        'https://www.okairos.gr/%CF%84%CF%81%CE%AF%CF%80%CE%BF%CE%BB%CE%B7.html?v=%CF%89%CF%81%CE%B9%CE%B1%CE%AF%CE%B1']
+    allowed_domains = ['https://www.okairos.gr/τρίπολη.html?v=ωριαία']
+    start_urls = ['https://www.okairos.gr/τρίπολη.html?v=ωριαία']
 
-    def bofortToKm(self, b):
+    @staticmethod
+    def bofortToKm(b):
         switcher = {
             0: 1,
             1: 3.5,
@@ -51,8 +50,10 @@ class OkairosSpiderSpider(scrapy.Spider):
 
         source = 'okairos.gr'
         city = 'Tripoli'
-        # time         = datetime.datetime(int(timestr[6:10]), int(timestr[3:5]), int(timestr[0:2]),int(timestr[11:13]),int(timestr[14:16]))  -  datetime.timedelta(hours=2, minutes=0)
-        hour = response.xpath('//*[@class="wnfp"]/table[1]/tr[2]/td[1]/text()').get()[0:2]
+        # time         = datetime.datetime(int(timestr[6:10]), int(timestr[3:5]), int(timestr[0:2]),
+        # int(timestr[11:13]),int(timestr[14:16]))  -  datetime.timedelta(hours=2, minutes=0)
+        # hour = response.xpath('//*[@class="wnfp"]/table[1]/tr[2]/td[1]/text()').get()[0:2]
+        hour = response.xpath('//*[@class="wnfp"]/table[1]//tr[3]/td[1]/text()').get()
         hour = int(self.timeStrToInt(hour))
         crawldate = dt.now()
         if hour == 0:
@@ -66,24 +67,22 @@ class OkairosSpiderSpider(scrapy.Spider):
         time = dt(year, month, day, int(hour), 0) - datetime.timedelta(hours=3, minutes=0)
         timeutc = dt(year, month, day, int(hour), 0)
         timestr = timeutc.strftime("%d/%m/%Y, %H:%M")
-        temperature = float(response.xpath('//*[@class="wnfp"]/table[1]/tr[2]/td[3]/div/text()').get()[:-1])
-        humidity = float(response.xpath('//*[@class="wnfp"]/table[1]/tr[2]/td[9]/text()').get()[:-1])
+        temperature = float(response.xpath('//*[@class="wnfp"]/table[1]/tr[3]/td[3]/div/text()').get()[:-1])
+        humidity = float(response.xpath('//*[@class="wnfp"]/table[1]/tr[3]/td[9]/text()').get()[:-1])
         # windends    = (rows[6].xpath('td//text()')[4].extract()).find(" ")
         # winddire    = (rows[6].xpath('td//text()')[4].extract()).find("at")
         barometer = float(
-            response.xpath('//*[@class="wnfp"]/table[1]/tr[2]/td[10]/text()').get().replace("\n", "").replace("\t", "")[
-            :-4])
+            response.xpath('//*[@class="wnfp"]/table[1]/tr[3]/td[10]/text()').get().replace("\n", "").replace("\t", "")[:-4])
         yetos = float(
-            response.xpath('//*[@class="wnfp"]/table[1]/tr[2]/td[7]/text()').get().replace("\t", "").replace(",", ".")[
-            :-2])
+            response.xpath('//*[@class="wnfp"]/table[1]/tr[3]/td[7]/text()').get().replace("\t", "").replace(",", ".")[:-2])
         direction = 'null'
-        b = int(response.xpath('//*[@class="wnfp"]/table[1]/tr[2]/td[5]/text()').get().strip())
+        b = int(response.xpath('//*[@class="wnfp"]/table[1]/tr[3]/td[5]/text()').get().strip())
         wind = float(self.bofortToKm(b))
         # barends     = rows[7].xpath('td//text()')[4].extract().find(" ")
 
-        id = source + ' ' + timestr
+        d_id = source + ' ' + timestr
         item = OkairosItem()
-        item["id"] = id
+        item["id"] = d_id
         item["source"] = source
         item["time"] = time
         item["timecrawl"] = crawldate
@@ -97,6 +96,4 @@ class OkairosSpiderSpider(scrapy.Spider):
         yield item
 
     def start_requests(self):
-        yield scrapy.Request(
-            'https://www.okairos.gr/%CF%84%CF%81%CE%AF%CF%80%CE%BF%CE%BB%CE%B7.html?v=%CF%89%CF%81%CE%B9%CE%B1%CE%AF%CE%B1',
-            self.parse)
+        yield scrapy.Request('https://www.okairos.gr/τρίπολη.html?v=ωριαία', self.parse)

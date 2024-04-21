eb1c5604fc3e15bca04303cc7289f9cde60467d1
Vasilis Koulouris <basileskouloures@gmail.com>
2024-04-21 11:48:47 +0300

    get dates

--- weather/spiders/xalazi_spider.py
+++ weather/spiders/xalazi_spider.py
@@ -1,24 +1,6 @@
 import scrapy
 from datetime import datetime as dt
-
-
-def bofortToKm(b):
-    switcher = {
-        0: 1,
-        1: 3.5,
-        2: 8,
-        3: 16,
-        4: 25,
-        5: 33,
-        6: 45,
-        7: 56,
-        8: 69,
-        9: 80,
-        10: 96,
-        11: 110,
-        12: 124
-    }
-    return switcher.get(b, 0)
+from ..functions import bofortToKm
 
 
 def timeStrToInt(b):
@@ -50,31 +32,29 @@ class XalaziSpider(scrapy.Spider):
     start_urls = ['http://www.xalazi.gr/prognwsh-kairou/prognosi-5-imeron?type=FiveDays&city=1178']
 
     def parse(self, response):
-
         source = 'xalazi.gr'
-        city = 'Tripoli'
+        city = response.xpath('//div[@class="top"]/div[@class="name"]/text()').get().split()[4]
+
+        counter = 1
+        date = ""
+        for table_row in response.xpath('//table[@class="t orangered"]//tr').getall():
+            try:
+                date = response.xpath('//table[@class="t orangered"]/tr[' + str(counter) + ']/td[1]/text()')[
+                    1].get().replace(
+                    "\r\n", "")
+            except IndexError:
+                pass  # do nothing
+
+            print(date)
+            counter += 1
+
         hour = response.xpath('//*[@class="t orangered"]/tr[1]/td[2]//text()').get().replace("\r", "").replace("\n",
                                                                                                                "").strip()[
                :2]
-        hour = int(timeStrToInt(hour))
-        crawldate = dt.now()
-        if (hour == 0):
-            cdate = dt.now()  # +  datetime.timedelta(days = 1)
-        else:
-            cdate = dt.now()
-        year = int(cdate.year)
-        month = int(timeStrToInt(
-            response.xpath('//*[@class="t orangered"]/tr[1]/td[1]//text()')[1].get().replace("\r", "").replace("\n",
-                                                                                                               "").strip()[
-            3:]))
-        day = int(timeStrToInt(
-            response.xpath('//*[@class="t orangered"]/tr[1]/td[1]//text()')[1].get().replace("\r", "").replace("\n",
-                                                                                                               "").strip()[
-            :2]))
 
-        time = dt(year, month, day, int(hour), 0) - datetime.timedelta(hours=3, minutes=0)
-        timeutc = dt(year, month, day, int(hour), 0)
-        timestr = timeutc.strftime("%d/%m/%Y, %H:%M")
+        day = response.xpath('//table[@class="t orangered"]/tr[1]/td[1]/text()')[1].get().replace("\r", "").replace(
+            "\n", "").replace(" ", "")
+
         temperature = float(
             response.xpath('//*[@class="t orangered"]/tr[1]/td[3]//text()').get().replace("\r", "").replace("\n",
                                                                                                             "").strip()[
@@ -95,12 +75,12 @@ class XalaziSpider(scrapy.Spider):
             response.xpath('//*[@class="t orangered"]/tr[1]/td[5]//text()').get().replace("\r", "").replace("\n", "")[
             :windindex].strip())
         wind = float(bofortToKm(b))
-
+        """
         yield {
             'src': source,
             'city': city,
             'timecrawl': dt.now(),
-            'day': convertDay(day),
+            'day': day,
             'hour': hour,
             'temperature': temperature,
             'wind_km': wind,
@@ -108,7 +88,9 @@ class XalaziSpider(scrapy.Spider):
             'barometer': barometer,
             'yetos': yetos
         }
+        """
 
     def start_requests(self):
+        # Τρίπολη
         yield scrapy.Request('http://www.xalazi.gr/prognwsh-kairou/prognosi-5-imeron?type=FiveDays&city=1178',
                              self.parse)
